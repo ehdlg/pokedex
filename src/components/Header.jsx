7304a8e93f49612f543c3376c2b3c56ca4c12bb8
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-10-10 08:47:41 +0200

    Header component that displays the header and gen select filter

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+function Header({ generation, setGeneration }) {
+  const NUM_GEN = 9;
+
+  const handleChangeGen = (e) => {
+    setGeneration(e.target.value);
+  };
+
+  const genOptions = [];
+  for (let i = 1; i <= NUM_GEN; i++) {
+    genOptions.push(<option key={i} value={i}>{`Generation ${i}`}</option>);
+  }
+  genOptions.push(
+    <option key={NUM_GEN + 1} value={'all'}>
+      {'All'}
+    </option>
+  );
+  return (
+    <header>
+      <h1>hello world</h1>
+
+      <select
+        name='genertion'
+        id='generation'
+        value={generation}
+        onChange={handleChangeGen}
+      >
+        <option value=''></option>
+        {genOptions.map((option) => option)}
+      </select>
+    </header>
+  );
+}
+
+export default Header;
