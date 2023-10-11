02186ffa8f4c992da2fab596a2305223430c8a59
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-10-11 10:38:37 +0200

    Added pokemon type info in a div

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -2,23 +2,33 @@
 // eslint-disable-next-line react/prop-types
 function Pokemon({ pokemon }) {
   return (
-    <div className='pokemon'>
+    <div className='pokemon pokeball-bkg'>
       <img
         className='pokemon-img'
         src={pokemon.img}
         alt={`${pokemon.name} sprite img from pokeAPI`}
         loading='lazy'
       />
-      <h5>
-        #
-        {pokemon.number < 10
-          ? `00${pokemon.number}`
-          : pokemon.number < 100
-          ? `0${pokemon.number}`
-          : pokemon.number}
-      </h5>
+      <div className='pokemon-data'>
+        <h6>
+          #
+          {pokemon.number < 10
+            ? `00${pokemon.number}`
+            : pokemon.number < 100
+            ? `0${pokemon.number}`
+            : pokemon.number}
+        </h6>
 
-      <h4 className='pokemon-name'>{pokemon.name}</h4>
+        <div className='types'>
+          {pokemon.types.map((type, index) => {
+            return (
+              <div key={index} className={`type type-${type.type.name}`}>
+                <span>{type.type.name}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
